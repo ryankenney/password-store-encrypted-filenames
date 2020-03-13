c70d7693409d08b8e2715ecdbf7462696eb3cbab
Ryan Kenney <ryan@ryankenney.info>
2020-03-13 16:13:32 -0700

    Fixed tabs

--- test-cases.sh
+++ test-cases.sh
@@ -15,24 +15,24 @@ echoTestStart() {
 }
 
 setup_test_files() {
-    echo -n "12345" > "$INDEX_SALT_FILE"
-    rm -f "$INDEX_FILE"
-    echo "/a/b/c/1.gpg" >> "$INDEX_FILE"
-    echo "/a/2.gpg" >> "$INDEX_FILE"
-    echo "/a/b/3.gpg" >> "$INDEX_FILE"
-    echo "/a/b/5.gpg/z/5.gpg" >> "$INDEX_FILE"
-    echo "/z/a/b/c/1.gpg" >> "$INDEX_FILE"
-    echo "/z/a/2.gpg" >> "$INDEX_FILE"
-    echo "/z/a/b/3.gpg" >> "$INDEX_FILE"
-    echo "/z/a/b/4.gpg" >> "$INDEX_FILE"
+	echo -n "12345" > "$INDEX_SALT_FILE"
+	rm -f "$INDEX_FILE"
+	echo "/a/b/c/1.gpg" >> "$INDEX_FILE"
+	echo "/a/2.gpg" >> "$INDEX_FILE"
+	echo "/a/b/3.gpg" >> "$INDEX_FILE"
+	echo "/a/b/5.gpg/z/5.gpg" >> "$INDEX_FILE"
+	echo "/z/a/b/c/1.gpg" >> "$INDEX_FILE"
+	echo "/z/a/2.gpg" >> "$INDEX_FILE"
+	echo "/z/a/b/3.gpg" >> "$INDEX_FILE"
+	echo "/z/a/b/4.gpg" >> "$INDEX_FILE"
 }
 
 encrypted_paths__path_to_hash() {
-    local saltfile="$1"
-    local passfile="$2"
-    local salt="$(cat "$saltfile")"
-    local host="$(echo -n "${salt}:${passfile}" | sha256sum )"
-    echo "${host:0:64}"
+	local saltfile="$1"
+	local passfile="$2"
+	local salt="$(cat "$saltfile")"
+	local host="$(echo -n "${salt}:${passfile}" | sha256sum )"
+	echo "${host:0:64}"
 }
 
 encrypted_paths__list_paths() {
@@ -48,7 +48,7 @@ encrypted_paths__list_paths() {
 
 encrypted_paths__remove_from_index() {
 	local prefix_filter="$1"
-    echo -n "" > "$INDEX_FILE.tmp"
+	echo -n "" > "$INDEX_FILE.tmp"
 	while read -r -d "" passfile; do
 		prefix_filter_len=$(echo -n "$prefix_filter" | wc -m)
 		passfile_chopped="${passfile:0:${prefix_filter_len}}"
@@ -65,7 +65,7 @@ encrypted_paths__is_in_index() {
 	while read -r -d "" passfile; do
 		if [[ "$target_passfile" == "$passfile" ]]; then
 			echo "true"
-            return
+			return
 		fi
 	done < <(cat "$INDEX_FILE" | sort -u | tr '\n' '\0')
 	echo "false"
@@ -73,121 +73,121 @@ encrypted_paths__is_in_index() {
 
 encrypted_paths__add_to_index() {
 	local target_passfile="$1"
-    cp "$INDEX_FILE" "$INDEX_FILE.tmp"
-    echo "$target_passfile" >> "$INDEX_FILE.tmp"
-    cat "$INDEX_FILE.tmp" | sort -u > "$INDEX_FILE.tmp.tmp"
-    mv "$INDEX_FILE.tmp.tmp" "$INDEX_FILE"
-    rm -f "$INDEX_FILE.tmp"
+	cp "$INDEX_FILE" "$INDEX_FILE.tmp"
+	echo "$target_passfile" >> "$INDEX_FILE.tmp"
+	cat "$INDEX_FILE.tmp" | sort -u > "$INDEX_FILE.tmp.tmp"
+	mv "$INDEX_FILE.tmp.tmp" "$INDEX_FILE"
+	rm -f "$INDEX_FILE.tmp"
 }
 
 test__encrypted_paths__list_paths() {
 
-    # Setup
-    echoTestStart "test__encrypted_paths__list_paths"
-    setup_test_files
-    local output_file="$SCRIPT_DIR/test__encrypted_paths__list_paths"
-    rm -f "$output_file.actual"
+	# Setup
+	echoTestStart "test__encrypted_paths__list_paths"
+	setup_test_files
+	local output_file="$SCRIPT_DIR/test__encrypted_paths__list_paths"
+	rm -f "$output_file.actual"
 
-    # Execute
-    encrypted_paths__list_paths "/a/b" > "${output_file}.actual"
+	# Execute
+	encrypted_paths__list_paths "/a/b" > "${output_file}.actual"
 
-    # Verify
-    diff "${output_file}.actual" "${output_file}.expected"
-    echo "RESULT: [SUCCESS]"
+	# Verify
+	diff "${output_file}.actual" "${output_file}.expected"
+	echo "RESULT: [SUCCESS]"
 }
 
 test__encrypted_paths__is_in_index() {
-    # Setup
-    echoTestStart "test__encrypted_paths__is_in_index"
-    setup_test_files
-    local output_file="$SCRIPT_DIR/test__encrypted_paths__is_in_index"
-    rm -f "$output_file.actual"
+	# Setup
+	echoTestStart "test__encrypted_paths__is_in_index"
+	setup_test_files
+	local output_file="$SCRIPT_DIR/test__encrypted_paths__is_in_index"
+	rm -f "$output_file.actual"
 
-    # Execute
-    echo "# Directories should not match" >> "${output_file}.actual"
-    encrypted_paths__is_in_index "/a/b" >> "${output_file}.actual"
-    echo "# Existing files should match" >> "${output_file}.actual"
-    encrypted_paths__is_in_index "/a/b/c/1.gpg" >> "${output_file}.actual"
-    encrypted_paths__is_in_index "/a/2.gpg" >> "${output_file}.actual"
-    encrypted_paths__is_in_index "/a/b/3.gpg" >> "${output_file}.actual"
-    echo "# Paths that are suffixes to others shouldn't match" >> "${output_file}.actual"
-    encrypted_paths__is_in_index "/z/a/b/4.gpg" >> "${output_file}.actual"
-    encrypted_paths__is_in_index "/a/b/4.gpg" >> "${output_file}.actual"
-    echo "# Paths that are prefixes to others shouldn't match" >> "${output_file}.actual"
-    encrypted_paths__is_in_index "/a/b/5.gpg/z/5.gpg" >> "${output_file}.actual"
-    encrypted_paths__is_in_index "/a/b/5.gpg" >> "${output_file}.actual"
+	# Execute
+	echo "# Directories should not match" >> "${output_file}.actual"
+	encrypted_paths__is_in_index "/a/b" >> "${output_file}.actual"
+	echo "# Existing files should match" >> "${output_file}.actual"
+	encrypted_paths__is_in_index "/a/b/c/1.gpg" >> "${output_file}.actual"
+	encrypted_paths__is_in_index "/a/2.gpg" >> "${output_file}.actual"
+	encrypted_paths__is_in_index "/a/b/3.gpg" >> "${output_file}.actual"
+	echo "# Paths that are suffixes to others shouldn't match" >> "${output_file}.actual"
+	encrypted_paths__is_in_index "/z/a/b/4.gpg" >> "${output_file}.actual"
+	encrypted_paths__is_in_index "/a/b/4.gpg" >> "${output_file}.actual"
+	echo "# Paths that are prefixes to others shouldn't match" >> "${output_file}.actual"
+	encrypted_paths__is_in_index "/a/b/5.gpg/z/5.gpg" >> "${output_file}.actual"
+	encrypted_paths__is_in_index "/a/b/5.gpg" >> "${output_file}.actual"
 
-    # Verify
-    diff "${output_file}.actual" "${output_file}.expected"
-    echo "RESULT: [SUCCESS]"
+	# Verify
+	diff "${output_file}.actual" "${output_file}.expected"
+	echo "RESULT: [SUCCESS]"
 }
 
 test__encrypted_paths__remove_from_index() {
 
-    # Setup
-    echoTestStart "test__encrypted_paths__remove_from_index"
-    setup_test_files
-    local output_file="$SCRIPT_DIR/test__encrypted_paths__remove_from_index"
-    rm -f "$output_file.actual"
+	# Setup
+	echoTestStart "test__encrypted_paths__remove_from_index"
+	setup_test_files
+	local output_file="$SCRIPT_DIR/test__encrypted_paths__remove_from_index"
+	rm -f "$output_file.actual"
 
-    # Execute
-    encrypted_paths__remove_from_index "/a/b"
+	# Execute
+	encrypted_paths__remove_from_index "/a/b"
 
-    # Verify
-    diff "$INDEX_FILE" "${output_file}.expected"
-    echo "RESULT: [SUCCESS]"
+	# Verify
+	diff "$INDEX_FILE" "${output_file}.expected"
+	echo "RESULT: [SUCCESS]"
 }
 
 test__encrypted_paths__add_to_index() {
 
-    # Setup
-    echoTestStart "test__encrypted_paths__add_to_index"
-    setup_test_files
-    local output_file="$SCRIPT_DIR/test__encrypted_paths__add_to_index"
-    rm -f "$output_file.actual"
+	# Setup
+	echoTestStart "test__encrypted_paths__add_to_index"
+	setup_test_files
+	local output_file="$SCRIPT_DIR/test__encrypted_paths__add_to_index"
+	rm -f "$output_file.actual"
 
-    # Execute
-    echo "# Adding an existing path has no effect" >> "${output_file}.actual"
-    encrypted_paths__add_to_index "/a/b/3.gpg"
-    cat "$INDEX_FILE" >> "${output_file}.actual"
-    echo "# Adding an entry that happens to be a suffix to another entry results in new entry" >> "${output_file}.actual"
-    encrypted_paths__add_to_index "/a/b/4.gpg"
-    cat "$INDEX_FILE" >> "${output_file}.actual"
-    echo "# Adding an entry that happens to be a prefix to another entry results in new entry" >> "${output_file}.actual"
-    encrypted_paths__add_to_index "/a/b/5.gpg"
-    cat "$INDEX_FILE" >> "${output_file}.actual"
+	# Execute
+	echo "# Adding an existing path has no effect" >> "${output_file}.actual"
+	encrypted_paths__add_to_index "/a/b/3.gpg"
+	cat "$INDEX_FILE" >> "${output_file}.actual"
+	echo "# Adding an entry that happens to be a suffix to another entry results in new entry" >> "${output_file}.actual"
+	encrypted_paths__add_to_index "/a/b/4.gpg"
+	cat "$INDEX_FILE" >> "${output_file}.actual"
+	echo "# Adding an entry that happens to be a prefix to another entry results in new entry" >> "${output_file}.actual"
+	encrypted_paths__add_to_index "/a/b/5.gpg"
+	cat "$INDEX_FILE" >> "${output_file}.actual"
 
-    # Verify
-    diff "${output_file}.actual" "${output_file}.expected"
-    echo "RESULT: [SUCCESS]"
+	# Verify
+	diff "${output_file}.actual" "${output_file}.expected"
+	echo "RESULT: [SUCCESS]"
 }
 
 test__encrypted_paths__path_to_hash() {
 
-    # Setup
-    echoTestStart "test__encrypted_paths__path_to_hash"
-    setup_test_files
-    local output_file="$SCRIPT_DIR/test__encrypted_paths__path_to_hash"
-    rm -f "$output_file.actual"
+	# Setup
+	echoTestStart "test__encrypted_paths__path_to_hash"
+	setup_test_files
+	local output_file="$SCRIPT_DIR/test__encrypted_paths__path_to_hash"
+	rm -f "$output_file.actual"
 
-    # Execute
-    echo "# Trying some paths" >> "${output_file}.actual"
-    encrypted_paths__path_to_hash "$INDEX_SALT_FILE" "/a/b/c/1.gpg" >> "${output_file}.actual"
-    encrypted_paths__path_to_hash "$INDEX_SALT_FILE" "/a/2.gpg" >> "${output_file}.actual"
-    encrypted_paths__path_to_hash "$INDEX_SALT_FILE" "/a/b/3.gpg" >> "${output_file}.actual"
-    echo "# Verify that salt has an effect" >> "${output_file}.actual"
-    mv "$INDEX_SALT_FILE" "${output_file}.salt"
-    echo -n "other" > "$INDEX_SALT_FILE"
-    encrypted_paths__path_to_hash "$INDEX_SALT_FILE" "/a/b/c/1.gpg" >> "${output_file}.actual"
-    encrypted_paths__path_to_hash "$INDEX_SALT_FILE" "/a/2.gpg" >> "${output_file}.actual"
-    encrypted_paths__path_to_hash "$INDEX_SALT_FILE" "/a/b/3.gpg" >> "${output_file}.actual"
-    mv "${output_file}.salt" "$INDEX_SALT_FILE"
-    echo "# Trying empty path" >> "${output_file}.actual"
-    encrypted_paths__path_to_hash "$INDEX_SALT_FILE" "" >> "${output_file}.actual"
+	# Execute
+	echo "# Trying some paths" >> "${output_file}.actual"
+	encrypted_paths__path_to_hash "$INDEX_SALT_FILE" "/a/b/c/1.gpg" >> "${output_file}.actual"
+	encrypted_paths__path_to_hash "$INDEX_SALT_FILE" "/a/2.gpg" >> "${output_file}.actual"
+	encrypted_paths__path_to_hash "$INDEX_SALT_FILE" "/a/b/3.gpg" >> "${output_file}.actual"
+	echo "# Verify that salt has an effect" >> "${output_file}.actual"
+	mv "$INDEX_SALT_FILE" "${output_file}.salt"
+	echo -n "other" > "$INDEX_SALT_FILE"
+	encrypted_paths__path_to_hash "$INDEX_SALT_FILE" "/a/b/c/1.gpg" >> "${output_file}.actual"
+	encrypted_paths__path_to_hash "$INDEX_SALT_FILE" "/a/2.gpg" >> "${output_file}.actual"
+	encrypted_paths__path_to_hash "$INDEX_SALT_FILE" "/a/b/3.gpg" >> "${output_file}.actual"
+	mv "${output_file}.salt" "$INDEX_SALT_FILE"
+	echo "# Trying empty path" >> "${output_file}.actual"
+	encrypted_paths__path_to_hash "$INDEX_SALT_FILE" "" >> "${output_file}.actual"
 
-    # Verify
-    diff "${output_file}.actual" "${output_file}.expected"
-    echo "RESULT: [SUCCESS]"
+	# Verify
+	diff "${output_file}.actual" "${output_file}.expected"
+	echo "RESULT: [SUCCESS]"
 }
 
 test__encrypted_paths__list_paths
